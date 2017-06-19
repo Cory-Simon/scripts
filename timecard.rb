78e638a3e16bdaa6e6216c58706ecca8d7246e9e
CORY SIMON <cory.simon@utah.edu>
2017-06-19 14:12:51 -0600

    added some todos

--- timecard.rb
+++ timecard.rb
@@ -22,3 +22,10 @@ def report
   total = ((morning + afternoon) / 60 / 60).round(2)
   "IN: #{@in.strftime('%H:%M')} (#{morning_total}) LUNCH: #{@lunch_out.strftime('%H:%M')}-#{@lunch_in.strftime('%H:%M')} (#{lunch}) OUT: #{@out.strftime('%H:%M')} TODAY: #{total}"
 end
+
+# TODO: lunch_in should report length of lunch
+# TODO: lunch_in should report hours worked so far
+# TODO: new method (hours) should report hours worked so far today
+# TODO: make this usable from terminal (not just pry)
+# TODO: persist the data between invocations
+# TODO: calculate the hours worked this week
